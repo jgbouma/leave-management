a83138b2ad1e78b95517c7f29227602d1c3f5a9c
Jan Gosse Bouma <jangossebouma@gmail.com>
2020-04-17 12:32:00 +0200

    Added Employee Leave Request View

---
+++ leave-management/Repository/LeaveRequestRepository.cs
@@ -0,0 +1,76 @@
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Repository
+{
+    public class LeaveRequestRepository : ILeaveRequestRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LeaveRequestRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool Create(LeaveRequest entity)
+        {
+            _db.LeaveRequests.Add(entity);
+            return Save();
+        }
+
+        public bool Delete(LeaveRequest entity)
+        {
+            _db.LeaveRequests.Remove(entity);
+            return Save();
+        }
+
+        public ICollection<LeaveRequest> FindAll()
+        {
+            var LeaveRequests = _db.LeaveRequests
+                .Include(q => q.RequestingEmployee)
+                .Include(q => q.ApprovedBy)
+                .Include(q => q.LeaveType)
+                .ToList();
+            return LeaveRequests;
+        }
+
+        public LeaveRequest FindById(int id)
+        {
+            var LeaveRequest = _db.LeaveRequests
+                .Include(q => q.RequestingEmployee)
+                .Include(q => q.ApprovedBy)
+                .FirstOrDefault(q => q.Id == id);
+            return LeaveRequest;
+        }
+
+        public ICollection<LeaveRequest> GetLeaveRequestsByEmployee(string employeeid)
+        {
+            var leaveRequests = FindAll()
+                .Where(q => q.RequestingEmployeeId == employeeid)
+                .ToList();
+            return leaveRequests;
+        }
+
+        public bool isExists(int id)
+        {
+            var exists = _db.LeaveRequests.Any(q => q.Id == id);
+            return exists;
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() > 0;
+        }
+
+        public bool Update(LeaveRequest entity)
+        {
+            _db.LeaveRequests.Update(entity);
+            return Save();
+        }
+    }
+}
